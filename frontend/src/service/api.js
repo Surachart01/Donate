4cdd5c15a11fe1b1d15938ee9e503e55deb09780
Surachart01 <113654372+Surachart01@users.noreply.github.com>
2025-03-15 00:31:37 +0700

    fixapiFrontend

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set up the API URL
-const API_URL = "http://localhost:4000/api";
+const API_URL = "https://donate-gtdg.onrender.com/api";
 const api = axios.create({
     baseURL: API_URL,
     headers: {
