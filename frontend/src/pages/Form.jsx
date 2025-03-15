9995ade3ef29554d4c69fbd81fedaf1ef9bd9638
Surachart01 <113654372+Surachart01@users.noreply.github.com>
2025-03-15 16:22:13 +0700

    scver3

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -89,7 +89,7 @@ const Form = () => {
         <div>
           {show &&<img src={IG} alt="" width={130} />}
           <div className="mt-3">
-            <h1 className="text-9xl text-white" style={{ fontFamily: "Outfit" }}>
+            <h1 className="text-9xl text-white" style={{ fontFamily: "Outfit" , wordWrap:"break-word" }}>
               {show?.igName || ""}
             </h1>
           </div>
