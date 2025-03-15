bc6c9fbcc9aee22f6c7e5e720d5097a84e972464
Surachart01 <113654372+Surachart01@users.noreply.github.com>
2025-03-15 10:12:29 +0700

    fix

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set up the API URL
-const API_URL = "https://9f34-202-14-164-197.ngrok-free.app/api";
+const API_URL = "https://f5d8-202-14-164-197.ngrok-free.app/api";
 const api = axios.create({
     baseURL: API_URL,
     headers: {
