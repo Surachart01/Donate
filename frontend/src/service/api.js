5a31ec6a165f76536f3216fcb70a299c59a69bac
toyozaza01 <toyoaz01@gmail.com>
2025-04-02 12:45:58 +0700

    Done API

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set up the API URL
-const API_URL = "http://localhost:4000/api";
+const API_URL = "https://3177-202-14-164-205.ngrok-free.app/api";
 const api = axios.create({
     baseURL: API_URL,
     headers: {
